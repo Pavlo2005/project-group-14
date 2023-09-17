229323a6be3c0cebfcaacd4f4c0f06aa0c1ea68d
Pavlo2005 <savyukpavlo8@gmail.com>
2023-09-17 21:30:53 +0200

    fix conflickt main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-
+import './js/01-header/01-header.js';
+import './js/02-introduction/02-introduction.js';
+import './js/03-categories/03-categories.js';
+import './js/04-popular-recips/04-popular-recips.js';
+import './js/05-filter/05-filter.js';
+import './js/06-dishes/06-dishes.js';
+import './js/07-menu/07-menu.js';
+import './js/08-dish-page/08-dish-page.js';
+import './js/09-favorites/09-favorites.js';
