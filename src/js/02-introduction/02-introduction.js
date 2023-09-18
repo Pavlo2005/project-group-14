4569175a02bb561b418c0339672da523433b1720
keugenich <mr.bumbastic.ek@gmail.com>
2023-09-18 20:00:35 +0100

    Борюсь зі swiper. Потрібна допомога

--- src/js/02-introduction/02-introduction.js
+++ src/js/02-introduction/02-introduction.js
@@ -1,47 +1,51 @@
 import Swiper from 'swiper';
+import 'swiper/css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
+
+    const baseUrl = 'https://tasty-treats-backend.p.goit.global/api';
+
+    // Дані із сервера
+    fetch(`${baseUrl}/events`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Метод map та шаблон
+            const slidesHTML = data.map(event => {
+                return `
+          <div class="swiper-slide">
+            <div class="chief">
+              <img class="chief-avatar" src="${event.cook.imgUrl}" alt="${event.cook.name}" />
+            </div>
+            <div class="preview">
+              <img class="preview-img" src="${event.topic.previewUrl}" alt="${event.topic.name} Preview" />
+              <h2 class="dish-title">${event.topic.name}</h2>
+              <p class="dish-origin">${event.topic.area}</p>
+            </div>
+            <div class="dish">
+              <img class="dish-img" src="${event.topic.imgUrl}" alt="${event.topic.name}" />
+            </div>
+          </div>
+        `;
+            });
 
 
+            swiperWrapper.innerHTML = slidesHTML.join('');
 
-const baseUrl = 'https://tasty-treats-backend.p.goit.global/api';
-
-fetch(`${baseUrl}/events`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then(data => {
-        const swiperWrapper = document.querySelector(".swiper-wrapper");
-
-        data.forEach(event => {
-            const slide = document.createElement("div");
-            slide.classList.add("swiper-slide");
-
-            slide.innerHTML = `
-                <div class="chief">
-                    <img class="chief-avatar" src="${event.cook.imgUrl}" alt="${event.cook.name}" />
-                </div>
-                <div class="dish">
-                    <img class="dish-img" src="${event.topic.imgUrl}" alt="${event.topic.name}" />
-                    <h2 class="dish-title">${event.topic.name}</h2>
-                    <p class="dish-origin">${event.topic.area}</p>
-                </div>
-                <div class="preview">
-                    <img class="preview-img" src="${event.topic.previewUrl}" alt="${event.topic.name} Preview" />
-                </div>
-            `;
-
-            swiperWrapper.appendChild(slide);
+            // Ініціалізація Swiper
+            new Swiper(".swiper-container", {
+                slidesPerView: 1,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            });
+        })
+        .catch(error => {
+            console.error("Помилка при отриманні даних з бекенду:", error);
         });
-
-        const swiper = new Swiper(".swiper", {
-            slidesPerView: 1,
-        });
-
-
-
-    })
-    .catch(error => {
-        console.error("Помилка при отриманні даних з бекенду:", error);
-    });
+});
