c40e7fe0ab215196a930e972d09403b1bb03fe08
evgen122 <evg.khristenko@gmail.com>
2023-09-21 20:44:36 +0300

    end

--- src/js/11-modal-reting/11-modal-reting.js
+++ src/js/11-modal-reting/11-modal-reting.js
@@ -1,146 +1,206 @@
-const btnGiveRating = document.querySelector('.js-open-rating-button');
+import Notiflix from 'notiflix';
+
 const windowPlace = document.querySelector('body');
+let refs;
+let idForratingRecips;
+let selectedRating = 1.0;
 
 const modalWindovs = `
-<div class="js-reting-overlay-modal reting-overlay-modal overlay " data-modal-reting>
-    <div class="reting-modal modal" data-modal="1">
-        <button type="button" class="js-modal-close reting-modal-close-btn">
-            <svg class="reting-modal-close-icon">
+<div class="js-rating-overlay-modal rating-overlay-modal overlay " data-modal-rating>
+    <div class="rating-modal modal" data-modal="1">
+        <button type="button" class="js-modal-close rating-modal-close-btn">
+            <svg class="rating-modal-close-icon">
                 <use href="/img/icon.svg#icon-cross"></use>
             </svg>
         </button>
-        <h2 class="reting-modal-title">Rating</h2>
-        <form class="js-reting-form-input reting-form-input " name="modal-form">
-          <div class="reting-form-star">
+        <h2 class="rating-modal-title">Rating</h2>
+        <form class="js-rating-form-input rating-form-input " name="modal-form">
+          <div class="rating-form-star">
 
-              <p class="reting-number">0.0</p>
-               <ul class="reting-list">
-                <li class="reting-item-personal">
+              <p class="js-rating-number rating-number">${selectedRating}</p>
+               <ul class="js-rating-list rating-list">
+                <li class="rating-item-personal">
                     <input class="star-rating__input" id="star-rating-5" type="radio" name="rating"
                         value="5" required >
                     <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-5"
-                        title="5 out of 5 stars"><svg class="reting-modal-star-icon ">
+                        title="5 out of 5 stars"><svg class="rating-modal-star-icon ">
                             <use href="/img/icon.svg#icon-star"></use>
                         </svg>
                     </label>
                 </li>
-                <li class="reting-item-personal">
+                <li class="rating-item-personal">
                     <input class="star-rating__input" id="star-rating-4" type="radio" name="rating"
                         value="4">
                     <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-4"
-                        title="4 out of 5 stars"><svg class="reting-modal-star-icon ">
+                        title="4 out of 5 stars"><svg class="rating-modal-star-icon ">
                             <use href="/img/icon.svg#icon-star"></use>
                         </svg></label>
                 </li>
-                <li class="reting-item-personal">
+                <li class="rating-item-personal">
                     <input class="star-rating__input" id="star-rating-3" type="radio" name="rating"
                         value="3">
                     <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-3"
-                        title="3 out of 5 stars"><svg class="reting-modal-star-icon ">
+                        title="3 out of 5 stars"><svg class="rating-modal-star-icon ">
                             <use href="/img/icon.svg#icon-star"></use>
                         </svg></label>
                 </li>
-                <li class="reting-item-personal">
+                <li class="rating-item-personal">
                     <input class="star-rating__input" id="star-rating-2" type="radio" name="rating"
                         value="2">
                     <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-2"
-                        title="2 out of 5 stars"><svg class="reting-modal-star-icon ">
+                        title="2 out of 5 stars"><svg class="rating-modal-star-icon ">
                             <use href="/img/icon.svg#icon-star"></use>
                         </svg></label>
                 </li>
-                <li class="reting-item-personal">
+                <li class="rating-item-personal">
                     <input class="star-rating__input" id="star-rating-1" type="radio" name="rating"
-                        value="1">
+                        value="1" checked >
                     <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-1"
-                        title="1 out of 5 stars"><svg class="reting-modal-star-icon ">
+                        title="1 out of 5 stars"><svg class="rating-modal-star-icon ">
                             <use href="/img/icon.svg#icon-star"></use>
                         </svg></label>
                 </li>
             </ul>
              
           </div>              
-            <input class="reting-modal-form-input" type="email" name="email"
-                  id="modal-email" placeholder="Enter email" required  />
+            <input class="js-rating-email rating-modal-form-input" type="email" name="email"
+                  id="modal-email" placeholder="Enter email" required/>
             
-            <button type="submit" class="js-reting-submit reting-button-submit reting-item-personal">Send</button>
+            <button type="submit" class="js-rating-submit rating-button-submit rating-item-personal">Send</button>
         </form>
     </div>
 </div>`;
 
-// btnGiveRating.addEventListener('click', handlerClick);
+// btnGiveRating.addEventListener('click', handlerClickReting);
 
-// ============================функція розмітки модального викна
+// ============================функція розмітки та виводу модального викна
 
-handlerClick();
+const id = '6462a8f74c3d0ddd28897fc1';
+// для проработки тимчасово
+handlerClickReting(id);
+// для проработки тимчасово
 
-function handlerClick() {
+function handlerClickReting(id) {
   // btnGiveRating.dataset.id;
+  //   const btnGiveRating = document.querySelector('.js-open-rating-button');
+  idForratingRecips = id;
 
   windowPlace.insertAdjacentHTML('beforeend', modalWindovs);
+  refs = {
+    // openModalBtn: document.querySelector('dataset.'),
+    close: document.querySelector('.js-modal-close'),
+    modal: document.querySelector('[data-modal-rating]'),
+    rating: document.querySelector('.js-rating-form-input'),
+    send: document.querySelector('.js-rating-submit'),
+    email: document.querySelector('.js-rating-email'),
+    ratingDisplay: document.querySelector('.js-rating-number'),
+    retingSelector: document.querySelector('.js-rating-list'),
+    //   close: document.querySelector('.js-modal-close'),
+    //   modalArea: document.querySelector('.js-rating-overlay-modal'),
+  };
+
+  //   toggleModal();
+  refs.modal.classList.toggle('rating-modal-is-hidden');
+
+  refs.retingSelector.addEventListener('click', retingWrite);
+  // selectedRating = document.querySelector("input[name='rating']:checked").value;
+
+  refs.send.addEventListener('click', handlerClickRet);
+  refs.close.addEventListener('click', handlerClickClose);
 }
 
-// ==================функція включення модального вікна
-const refs = {
-  // openModalBtn: document.querySelector('dataset.'),
-  closeModalBtn: document.querySelector('.js-modal-close'),
-  modal: document.querySelector('[data-modal-reting]'),
-  reting: document.querySelector('.js-reting-form-input'),
-  send: document.querySelector('.js-reting-submit'),
-  //   close: document.querySelector('.js-modal-close'),
-  modalArea: document.querySelector('.js-reting-overlay-modal'),
-};
-(() => {
-  //   const refs = {
-  //     openModalBtn: document.querySelector('dataset.'),
-  //     closeModalBtn: document.querySelector('.js-modal-close'),
-  //     modal: document.querySelector('[data-modal-reting]'),
-  //   };
+// ==================функція відображення вибраного рейтингу
+function retingWrite(elem) {
+  selectedRating = document.querySelector("input[name='rating']:checked").value;
+  console.log(selectedRating);
 
-  // refs.btnGiveRating.addEventListener('click', toggleModal);
-  //   refs.closeModalBtn.addEventListener('click', toggleModal);
-  toggleModal();
+  refs.ratingDisplay.innerHTML = `<p class="js-rating-number rating-number">${selectedRating}</p>`;
+}
+// ================================================================
 
-  function toggleModal() {
-    refs.modal.classList.toggle('reting-modal-is-hidden');
+// =================функція запиту на бекенд
+// serviceRating('6462a8f74c3d0ddd28897fbc')
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => console.log(err));
+// Запит на бекенд - щоразу потрібно міняти email або Id
+// інакше приходить повідомлення {message: 'Such email already exists'}
+// а якщо міняти зростає значення ключа whoRated, в об'єкті, що повертається
+
+async function serviceRating(Id, rating, email) {
+  const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+  const END_POINT = `/recipes/${Id}/rating`;
+  const postToUpdate = {
+    rate: rating,
+    email: email,
+  };
+
+  console.log('rating', postToUpdate.rate);
+  console.log('email', postToUpdate.email);
+
+  const options = {
+    method: 'PATCH',
+    body: JSON.stringify(postToUpdate),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const resp = await fetch(`${BASE_URL}${END_POINT}`, options);
+  if (!resp.statusText === 'OK') {
+    throw new Error('Error');
   }
-})();
-
-const enterReting = {
-  reting: document.querySelector('.js-reting-form-input'),
-  send: document.querySelector('.js-reting-submit'),
-  close: document.querySelector('.js-modal-close'),
-  modalArea: document.querySelector('.js-reting-overlay-modal'),
-};
-
-// enterReiting.reting.addEventListener();
-
-// const selectedReting = document.querySelector("input[name='rating']:checked");
-// console.log('se', selectedReting);
-// handlerClickReting();
-
-enterReting.send.addEventListener('click', handlerClickReting);
-enterReting.close.addEventListener('click', handlerClickClose);
+  return await resp.json();
+}
+// ================================================
 
 // ==================функція повернення вибору рейтингу
-function handlerClickReting(evt) {
-  console.log(4234234);
-  const selectedReting = document.querySelector(
-    "input[name='rating']:checked"
-  ).value;
-  console.log('se', selectedReting);
-  evt.preventDefault();
-  if (!selectedReting) {
-    console.log('Error data');
-    return 'Error data';
-  }
 
-  console.log(selectedReting);
-  //   return inputReting;
+async function handlerClickRet(evt) {
+  console.log(4234234);
+
+  selectedRating = Number(
+    document.querySelector("input[name='rating']:checked").value
+  );
+  // const email = document.querySelector('.js-rating-email').value;
+  const emailRating = refs.email.value;
+
+  console.log('rating', selectedRating);
+  console.log(typeof selectedRating);
+  console.log('email', emailRating);
+  console.log('id', idForratingRecips);
+
+  evt.preventDefault();
+
+  serviceRating(idForratingRecips, selectedRating, emailRating)
+    .then(data => {
+      console.log(data.message);
+      Notiflix.Notify.failure(data.message);
+    })
+    .catch(err => console.log(err));
+
+  // const resp = await fetch(
+  //   `https://tasty-treats-backend.p.goit.global/api/recipes/${idForratingRecips}/rating`
+  // );
+
+  // if (!resp.ok) {
+  //   return Promise.reject(resp.statusText);
+  // }
+
+  handlerClickClose();
+  //   data = await resp.json();
+  //   return data;
+  //   return inputrating;
 }
+
+// ==================================================
+
 //  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
 
 // ==================функція закриття модалки
 function handlerClickClose() {
-  refs.modal.classList.toggle('reting-modal-is-hidden');
-  refs.modalArea.innerHTML = '';
+  refs.modal.classList.toggle('rating-modal-is-hidden');
+  refs.modal.innerHTML = '';
 }
+
+export { handlerClickReting };
